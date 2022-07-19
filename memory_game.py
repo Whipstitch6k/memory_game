5e50202570838bec5547c496d935c8e17d715d96
Whipstitch6k <reese.lec@protonmail.com>
2022-07-19 18:33:03 -0500

    initial commit: basic planning complete

---
+++ memory_game.py
@@ -0,0 +1,8 @@
+#need a deck object, array of values
+  #constructor will initialize based on a defined grid size, pairs using letters to start
+#need a grid to be defined from deck after shuffle
+
+#need a game class
+  #gamestate variable (ongoing, finished), while gamestate == ongoing
+  #player object containing score
+  #turn method
