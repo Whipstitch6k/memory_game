851ca2492bcde6debbd46f481b5ba41515c0df6e
Whipstitch6k <reese.lec@protonmail.com>
2022-07-22 17:54:07 -0500

    gui version working. Needs a few tweaks and refactoring maybe

--- memory_game.py
+++ memory_game.py
@@ -1,6 +1,8 @@
 import random
 import time
-import os #for clear screen, currently uses cls
+import os #for console version clear screen, currently uses clear (unix)
+from tkinter import *    #using tikinter for the gui because it's simple and comes with python
+from tkinter import ttk
 
 class Card:
   def __init__(self, value):
@@ -23,6 +25,7 @@ class Game_grid:
 
   def initialze_game_grid(self, grid_size):
     #start by creating a deck, 1D list of paired Card objects (value is a character), in order (ie [A, A, B, B, C, C])
+    #the algorithm used does still generate workable grids when given an odd grid (ie a 3x3 grid has 9 spaces, but the algorithm creates 8 cards)
     deck = [];
     current_char = 'A'
     card_count = grid_size[0]*grid_size[1]
@@ -75,8 +78,8 @@ class Game_grid_console(Game_grid):
   #converts player_coord (ie "A2") into list_coord (ie [0,1])
   #note: as written only works for boards smaller than 26x10
   def convert_player_coord_to_list_coord(self, player_coord):
-    x_coord = ord(player_coord[0])-65
-    y_coord = int(player_coord[1])-1
+    x_coord = ord(player_coord[0])-65 #"A" is ascii 65
+    y_coord = int(player_coord[1])-1 #subtract to move from 1 base to 0 base
     return [x_coord,y_coord]
 
   #validates player_coord (ie "A2") is within board range (ie D3 on a 2x2 board would not be in range)
@@ -87,9 +90,6 @@ class Game_grid_console(Game_grid):
       return False
     else:
       return True
-
-class Game_grid_gui(Game_grid):
-  pass
 
 
 
@@ -213,10 +213,94 @@ class Memory_game_console(Memory_game):
 
 
 class Memory_game_gui(Memory_game):
-  pass
+  def __init__(self, player_count, grid_size):
+    super().__init__(player_count, grid_size)
+    #self.game_grid_object = Game_grid_gui(grid_size)
+    self.current_player = self.players[0]
+    self.current_player_phase = 0 # 0 = no selections yet, 1= one selection made, 2 = both selections made
+    self.current_player_selections = [] #list, of cards player selects this turn, returns to empty after turn
+    
+    #setup window and grid
+    self.root = Tk()
+    self.frame = ttk.Frame(self.root, padding=10) #create frame
+    self.frame.grid()
+
+
+  def play_game(self):
+
+    self.display_game_grid()
+
+    self.root.mainloop()
+
+
+  def display_game_grid(self):
+
+    for i in self.game_grid_object.game_grid:
+      for j in i:
+        if j.flipped:
+          displayed_card_value = j.value
+        else:
+          displayed_card_value = " "
+        button_coord = [i.index(j), self.game_grid_object.game_grid.index(i)]
+        ttk.Button(self.frame, text=displayed_card_value, command=lambda button_coord_to_pass = button_coord: self.advance_game_state(button_coord_to_pass)).grid(column=button_coord[0], row=button_coord[1])
+    ttk.Label(self.frame, text=self.current_player.name).grid(column=0, row=len(self.game_grid_object.game_grid[0])+1)
+    ttk.Label(self.frame, text="Score: " + str(self.current_player.score)).grid(column=0, row=len(self.game_grid_object.game_grid[0])+2)
+
+
+  def advance_game_state(self, grid_coord):
+    self.current_player_selections.append(self.game_grid_object.game_grid[grid_coord[1]][grid_coord[0]])
+    self.current_player_selections[-1].flipped = True
+    self.display_game_grid()
+
+    if self.current_player_phase == 1: #two selections have been made
+      print()
+      if self.current_player_selections[0].value == self.current_player_selections[1].value:
+        self.current_player.score += 1
+        self.current_player_selections[0].matched = True
+        self.current_player_selections[1].matched = True
+      else:
+        self.current_player_selections[0].flipped = False
+        self.current_player_selections[1].flipped = False
+        self.current_player = self.cycle_to_next_player(self.current_player)
+      
+      self.current_player_selections = [] #reset for next turn
+
+    self.advance_player_phase()
+
+    if self.game_grid_object.verify_all_matches_made():
+      self.finish_game()
+
+
+  def advance_player_phase(self):
+    self.current_player_phase += 1
+    if self.current_player_phase >1:
+      self.current_player_phase = 0
+
+
+  #display a new window with final scores
+  def finish_game(self):
+    root = Tk()
+    frm = ttk.Frame(root, padding=10)
+    frm.grid()
+    current_row = 1
+    ttk.Label(frm, text="Final Score:").grid(column=0, row=0)
+    for i in self.players:
+      ttk.Label(frm, text=i.name + ": " + str(i.score)).grid(column=0, row=current_row)
+      current_row += 1
+    root.mainloop()
 
 
 
 #create a new game with known grid size
-memory_game = Memory_game_console(1,[2,3])
+
+#switch these out to play console or gui
+#args are(number_of_players, [grid_width, grid_height])
+#memory_game = Memory_game_console(1,[2,3])
+memory_game = Memory_game_gui(1,[2,3])
+
 memory_game.play_game()
+
+
+#notes on gui: At current, intead of doing a delay after a mismatch and then flipping the cards back, it flips the cards back after the next selection. In a multi-player game this is also when it updates the player currently playing. Usage of sleeps seems to cause it to sleep before the second card reveal no matter where I put the sleep. Also gui does not yet handle re-selecting an already flipped card. 
+
+#console version is pretty solid but could be expanded on. Only works if inputs are a single letter followe dy a single number. Game won't work right if more wider than 9 columns
